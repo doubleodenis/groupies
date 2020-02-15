a96c9129d4e54ece43ce9eae0ecf8f4f92efabd8
Denis Ortega <ortega.dc98@gmail.com>
2020-02-15 00:57:30 -0500

    Added single voting functionality, and adding votes.

--- js/voting.js
+++ js/voting.js
@@ -6,7 +6,7 @@
 // }
 
 
-var polls = [{"creator": "Denis","name":"Poll 1","votes":{"vote1":["Denis", "Susan"],"vote2":["Susan"],"vote3":["Danny", "Denis", "Marcel"]},"timestamp":"1/13/2020"},{"name":"2","votes":{"vote1":["Denis", "Brian"],"vote2":["Denis", "Danny"],"vote3":["Denis"]},"timestamp":"1/13/2020"},{"name":"3","votes":{"vote1":["Denis"],"vote2":["Denis"],"vote3":["Denis", "Danny", "Kristen"],"vote4":["Denis", "Brian", "Abdiel"]},"timestamp":"1/13/2020"}];
+var polls = [{"creator": "Denis","name":"Poll 1","votes":{"This is a giant vote text":["Denis"],"vote2":["Susan"],"vote3":["Danny", "Marcel"]},"timestamp":"1/10/2020"},{"creator":"Susan","name":"2","votes":{"vote1":["Denis", "Brian"]},"timestamp":"1/13/2020"},{"name":"3","votes":{"vote1":["Brian"],"vote2":["Denis"],"vote3":["Danny", "Kristen"],"vote4":["Abdiel", "Susan"]},"timestamp":"1/13/2020"}];
 var currentGroupie = null;
 
 //Event Listeners
@@ -19,12 +19,15 @@ $('#groupie-selector').on("click", function(e) {
 
 //Add a new poll
 function addPoll() {
-    if(currentGroupie) {
-        const name = $('#add-poll').find('input').val();
+    const name = $('#add-poll').find('input').val();
+    const multi = false; //Get from checkbox
+    if(currentGroupie && name !== '') {
+        
         //LATER: Validate unique names of polls
         polls.push({
             name,
             creator: currentGroupie,
+            multi,
             votes: {},
             timestamp: new Date().getMonth().toString() + "/" + new Date().getDate().toString() + "/" + new Date().getFullYear()
         })
@@ -34,21 +37,46 @@ function addPoll() {
 }
 
 //Add vote to a poll
-function addVoteName(pollName, voteName) {
-    const poll = polls.find(poll => poll.name === pollName);
-    poll.votes[voteName] = [];
+function addVoteName(index) {
+    const voteName = $(`#poll${index} .add-vote`);
+    const val = voteName.val();
+    if(val && val !== "") {
+        const poll = polls[index];
+        poll.votes[val] = [];
+        _setup();
+        voteName.val("");
+    }
 }
 
 function openVote(index) {
 
 }
 //
-function vote(poll, voteKey) {
+function vote(pollIndex, voteKey) {
+    // console.log(poll, voteKey)
+    const poll = polls[pollIndex]
     if(currentGroupie !== null) {
         for(const key in poll.votes) {
-            if(voteKey === key && !poll.votes[key].includes(currentGroupie)) poll.votes[key].push(currentGroupie);
+
+            //Multi vote
+            if(poll.multi == true) {
+                //Need to handle unchecking checkboxes
+                if(voteKey === key && !poll.votes[key].includes(currentGroupie)) poll.votes[key].push(currentGroupie);
+            }
+            //Single vote
+            else {
+                // console.log(poll.votes[key].includes(currentGroupie))
+                if(voteKey !== key && poll.votes[key].includes(currentGroupie)) {
+                    const idx = poll.votes[key].indexOf(currentGroupie);
+                    console.log(currentGroupie, idx);
+                    if(idx > -1) poll.votes[key].splice(idx, 1);
+                }    
+                else if(voteKey === key && !poll.votes[key].includes(currentGroupie))
+                    poll.votes[key].push(currentGroupie);
+            }
         }
     }
+    _setup();
     
 }
 
@@ -67,7 +95,7 @@ function getPollByName(pollName) {
 
 function getPolls() {
     //Mock service
-    console.log(polls);
+    // console.log(polls);
 }
 
 function _setup() {
@@ -79,7 +107,8 @@ function _setup() {
         $('#poll-container').append(`
         <div class="poll" id="poll${index}">
             <div style="margin: 10px 0px;">
-                <span style="font-weight: bold; font-size: 20px;">${poll.name}</span> 
+                <span style="font-weight: bold; font-size: 20px;">${poll.name}</span>
+                by ${poll.creator} 
                 <span style="float: right">${poll.timestamp}</span>
             </div>
             <div class="voting-container">
@@ -88,21 +117,27 @@ function _setup() {
         </div>`);
         if(index == 0) $(".poll").toggleClass('main');
 
-        console.log(poll.votes)
+        // console.log(poll.votes)
         //use calc()
         for(const key in poll.votes) {
+            const checked = poll.votes[key].includes(currentGroupie)
             $(`#poll${index} .voting-container`).append(`
-                <div>
-                    <span>${key}</span>
-                    <div class="voting-bar" style="width: calc(100% - ${100 - Math.round(poll.votes[key].length/total * 100)}%);"></div>
+                <div class="tooltip">
+                    <input type="radio" onclick="vote(${index}, '${key}')" ${checked ? "checked" : ""}>
+                    <div class="ellipse">${key}</div>
+                    <div class="voting-bar" style="width: calc(${.75 * Math.round(poll.votes[key].length/total * 100)}%);"></div>
                     <span>${Math.round(poll.votes[key].length/total * 100)}%</span>
+                    <div class="tooltiptext" >
+                        <b style="margin-bottom: 5px">${key}</b>
+                        <span>Voters: ${poll.votes[key].join(", ")}</span>
+                    </div>
                 </div> 
             `);
         }
         if(poll.creator === currentGroupie) {
-            $(`#poll${index} .voting-container`).append(`
+            $(`#poll${index}`).append(`
                 <div>
-                    <input type="text" placeholder="New Vote Name..."style="border: 1px solid #cecece;">
+                    <input type="text" class="add-vote" placeholder="New Vote Name..."style="border: 1px solid #cecece;">
                     <button onclick="addVoteName(${index})" class="inverse-btn" style="box-shadow: none; ">+</button>
                 </div> 
             `);
